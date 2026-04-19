69ec4792e6b80847d985d9e98dcce7bcab114ad4
Alex <alex-96recre@hotmail.com>
2026-04-19 21:49:01 +0200

    Configuracion incial EF Core y Radzen

---
+++ Traza.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Traza.Web.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
--- Traza.web/Program.cs
+++ Traza.web/Program.cs
@@ -1,4 +1,7 @@
-using Traza.web.Components;
+using Microsoft.EntityFrameworkCore;
+using Radzen;
+using Traza.Web.Components;
+using Traza.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,16 +9,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddRadzenComponents();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
